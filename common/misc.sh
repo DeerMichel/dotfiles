12140268f4834147d7efe8c0a5294345854e402b
Micha Hanselmann <deermichel@github.com>
2019-06-29 23:46:06 -0700

    fix wrong path

--- common/misc.sh
+++ common/misc.sh
@@ -12,7 +12,7 @@ info "Installing dotfiles loader"
 mkdir -p ~/.dotfiles
 cp ./common/dotfiles_loader.sh ~/.dotfiles/loader.sh
 echo -e "\n# dotfiles loader" >> ~/.zshrc
-echo "[ -s \"~/.dotfiles/loader.sh\" ] && \. \"~/.dotfiles/loader.sh\"" >> ~/.zshrc
+echo "[ -s ~/.dotfiles/loader.sh ] && source ~/.dotfiles/loader.sh" >> ~/.zshrc
 
 # source current setup
 source ~/.zshrc
